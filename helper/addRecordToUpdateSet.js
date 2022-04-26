9320a6ac9a6927c4c5a99b71779488768b6ef186
Fabio Sacco <31451046+saccogf@users.noreply.github.com>
2022-04-26 18:05:19 -0300

    Update addRecordToUpdateSet.js

--- helper/addRecordToUpdateSet.js
+++ helper/addRecordToUpdateSet.js
@@ -2,16 +2,17 @@
  * Run this as a background script and change the table name and encoded query accordingly
  * Make sure you have an In Progress Update Set and it's set as your current
  * Also make sure the scope is valid, i.e. only add Global records if your Update Set is in Global scope
+ * @param {String} table - table name
+ * @param {String} encQuery - encoded query for the records to be added
  */
-var table = 'sys_attachment';
-var encQuery = 'sys_id=4d6d0d641b4381d47f9bfc8f034bcb9e';
+function addRecordToUpdateSet(table, encQuery) {
+    // Adds records above to the current update set
+    var updMgr = new GlideUpdateManager2();
 
-// Adds record above to the current update set
-var updMgr = new GlideUpdateManager2();
-
-var rec = new GlideRecord(table);
-rec.addEncodedQuery(encQuery);
-rec.query();
-while (rec.next()) {
-    updMgr.saveRecord(rec);
+    var rec = new GlideRecord(table);
+    rec.addEncodedQuery(encQuery);
+    rec.query();
+    while (rec.next()) {
+        updMgr.saveRecord(rec);
+    }
 }
